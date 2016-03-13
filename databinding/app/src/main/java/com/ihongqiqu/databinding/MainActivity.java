d01f1d6c093662a8e155c01f6f1d7d1bfc0420d7
Zhenguo <jingle1267@163.com>
2016-03-13 14:44:00 +0800

    [ADD] Add basic data binding.

--- databinding/app/src/main/java/com/ihongqiqu/databinding/MainActivity.java
+++ databinding/app/src/main/java/com/ihongqiqu/databinding/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ihongqiqu.databinding;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button2:
+                startDataBindingSampleActivity1();
+                break;
+            case R.id.button3:
+
+                break;
+            case R.id.button4:
+
+                break;
+        }
+    }
+
+    private void startDataBindingSampleActivity1() {
+        Intent intent = new Intent(this, DataBindingSampleActivity1.class);
+        startActivity(intent);
+    }
+
 }
